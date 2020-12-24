f84b6c203d02474f7b64a046b96c70bc5102a64a
ranisans <ranisans@yahoo.com>
2020-12-24 15:59:11 +0300

    Updates

---
+++ precache-manifest.611ed5a597d7b002b4b596de6e198874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8cfd4b4461ebd1f3a51cf0f166be7b",
+    "url": "/task_board/index.html"
+  },
+  {
+    "revision": "82872f14d21bda97185e",
+    "url": "/task_board/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "933671c8f10e050b6790",
+    "url": "/task_board/static/js/2.2f6fe2d7.chunk.js"
+  },
+  {
+    "revision": "27e28cccc1ec422e68316fd29acd149a",
+    "url": "/task_board/static/js/2.2f6fe2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82872f14d21bda97185e",
+    "url": "/task_board/static/js/main.fb0fe53f.chunk.js"
+  },
+  {
+    "revision": "33b1ac585452a3432faf",
+    "url": "/task_board/static/js/runtime-main.86ec5851.js"
+  }
+]);
